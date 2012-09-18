a4a2d2fb4eb3e15c0c93b648b2fb8425818aa33b
dead-horse <dead_horse@qq.com>
2012-09-18 15:10:57 +0800

    support block code

--- test/libs/util.js
+++ test/libs/util.js
@@ -13,14 +13,24 @@ var should = require('should');
 describe('libs/util', function() {
   describe('escape', function() {
     var text1 = '<script></script> text';
-    var text2 = 'outside:<>, inside: ```js\n<>\n```\n`<>`\n```\n<>\n```\n`uc` `uc`';
+    var text2 = 'outside:<>, inside: ```js\n<>\n```\n`<>`\n```\n<>\n```\n`span` `span`';
+    var text3 = '\t<>\n    <>\n';
+    var text4 = 'abc\n\t<>\n\t<>';
     it('escape outside ok', function() {
       var result = Util.escape(text1);
       result.should.equal('&lt;script&gt;&lt;/script&gt; text');
     });
     it('not escape inside', function() {
       var result = Util.escape(text2);
-      result.should.equal('outside:&lt;&gt;, inside: ```js\n<>\n```\n`<>`\n```\n<>\n```\n`uc` `uc`');
+      result.should.equal('outside:&lt;&gt;, inside: ```js\n<>\n```\n`<>`\n```\n<>\n```\n`span` `span`');
+    });
+    it('not escape inside block', function() {
+      var result = Util.escape(text3);
+      result.should.equal('\t<>\n    <>\n');
+    });
+    it('escape not inside', function() {
+      var result = Util.escape(text4);
+      result.should.equal('abc\n\t&lt;&gt;\n\t&lt;&gt;');
     });
   });
 });
