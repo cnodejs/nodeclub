360317ec90cbaf0bcc8694abf81296ba91add1ea
Alsotang <alsotang@gmail.com>
2014-01-04 01:23:14 +0800

    Fixed #265
    很多人的 GitHub 没有提供公开的 Email 地址，导致注册失败。

--- controllers/github.js
+++ controllers/github.js
@@ -54,7 +54,8 @@ exports.create = function (req, res, next) {
         if (err.err.indexOf('duplicate key error') !== -1) {
           if (err.err.indexOf('users.$email') !== -1) {
             return res.status(500)
-              .send('您 GitHub 账号的 Email 与之前在 CNodejs 注册的 Email 重复了');
+              .send('您 GitHub 账号的 Email 与之前在 CNodejs 注册的 Email 重复了，\
+也可能是您的 GitHub 没有提供公开的 Email 导致注册失败。');
           }
           if (err.err.indexOf('users.$loginname') !== -1) {
             return res.status(500)
