17ab29cc45144a887f9a6a67d79fcf334ff3fafa
李凡 <lf2941270@163.com>
2014-02-25 10:29:26 +0800

    1

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.configure(function () {
 app.configure('development', function () {
   app.use('/public', express.static(staticDir));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 });
 
 app.configure('production', function () {
