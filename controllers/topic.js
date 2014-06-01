c3189eb12e9dec738c7d5870848f873209477971
Wang Hao <eric.3126@hotmail.com>
2014-06-01 13:21:40 +0800

    Bug Fixed: Redirect `home` to `/`
    Since express has   `Removed `res.redirect('home')` support ` after the
    version `3.0.0alpha1 / 2012-04-15`, It may lead a 404 error in some
    error handling situation.

--- controllers/topic.js
+++ controllers/topic.js
@@ -155,7 +155,7 @@ exports.put = function (req, res, next) {
 
 exports.showEdit = function (req, res, next) {
   if (!req.session.user) {
-    res.redirect('home');
+    res.redirect('/');
     return;
   }
 
@@ -192,7 +192,7 @@ exports.showEdit = function (req, res, next) {
 
 exports.update = function (req, res, next) {
   if (!req.session.user) {
-    res.redirect('home');
+    res.redirect('/');
     return;
   }
   var topic_id = req.params.tid;
@@ -328,7 +328,7 @@ exports.delete = function (req, res, next) {
 
 exports.top = function (req, res, next) {
   if (!req.session.user.is_admin) {
-    res.redirect('home');
+    res.redirect('/');
     return;
   }
   var topic_id = req.params.tid;
