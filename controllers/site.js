b767463f6bbd360b97491646f5eb105748d4cbc2
Raven WANG <ravenwang@gmail.com>
2014-08-12 01:09:27 +0800

    实现隐藏当前用户屏蔽对象所发表的主题

--- controllers/site.js
+++ controllers/site.js
@@ -9,7 +9,9 @@
  * Module dependencies.
  */
 
+var _ = require('lodash');
 var User = require('../proxy').User;
+var Relation = require('../proxy').Relation;
 var Topic = require('../proxy').Topic;
 var config = require('../config').config;
 var EventProxy = require('eventproxy');
@@ -20,9 +22,9 @@ setInterval(function () {
   var limit = config.list_topic_count;
   // 只缓存第一页, page = 1
   var options = { skip: (1 - 1) * limit, limit: limit, sort: [ ['top', 'desc' ], [ 'last_reply_at', 'desc' ] ] };
-  var optionsStr = JSON.stringify(options);
+  var cacheKey = JSON.stringify([{}, options]);
   Topic.getTopicsByQuery({}, options, function (err, topics) {
-    mcache.put(optionsStr, topics);
+    mcache.put(cacheKey, topics);
     return topics;
   });
 }, 1000 * 5); // 五秒更新一次
@@ -42,21 +44,65 @@ exports.index = function (req, res, next) {
         tops: tops,
         no_reply_topics: no_reply_topics,
         pages: pages,
-        site_links: config.site_links,
+        site_links: config.site_links
       });
     });
   proxy.fail(next);
 
-  // 取主题
-  var options = { skip: (page - 1) * limit, limit: limit, sort: [ ['top', 'desc' ], [ 'last_reply_at', 'desc' ] ] };
-  var optionsStr = JSON.stringify(options);
-  if (mcache.get(optionsStr)) {
-    proxy.emit('topics', mcache.get(optionsStr));
+  proxy.on('blockings', function(blockIds) {
+    var query = {};
+    if (blockIds && blockIds.length > 0) {
+      query = {author_id: {$nin: blockIds}};
+    }
+
+    // 取主题
+    var options = { skip: (page - 1) * limit, limit: limit, sort: [ ['top', 'desc' ], [ 'last_reply_at', 'desc' ] ] };
+    var topicsCacheKey = JSON.stringify([query, options]);
+    if (mcache.get(topicsCacheKey)) {
+      proxy.emit('topics', mcache.get(topicsCacheKey));
+    } else {
+      Topic.getTopicsByQuery(query, options, proxy.done('topics', function (topics) {
+        return topics;
+      }));
+    }
+
+    // 取分页数据
+    var pagesCacheKey = JSON.stringify([query, 'pages']);
+    if (mcache.get(pagesCacheKey)) {
+      proxy.emit('pages', mcache.get(pagesCacheKey));
+    } else {
+      Topic.getCountByQuery(query, proxy.done(function (all_topics_count) {
+        var pages = Math.ceil(all_topics_count / limit);
+        mcache.put(pagesCacheKey, pages, 1000 * 60 * 1);
+        proxy.emit('pages', pages);
+      }));
+    }
+
+    // 取0回复的主题
+    if (mcache.get('no_reply_topics')) {
+      proxy.emit('no_reply_topics', mcache.get('no_reply_topics'));
+    } else {
+      Topic.getTopicsByQuery(
+        _.assign({ reply_count: 0 }, query),
+        { limit: 5, sort: [ [ 'create_at', 'desc' ] ] },
+        proxy.done('no_reply_topics', function (no_reply_topics) {
+          mcache.put('no_reply_topics', no_reply_topics, 1000 * 60 * 1);
+          return no_reply_topics;
+        }));
+    }
+  });
+
+  if (req.session.user) {
+    Relation.getBlockings(
+      req.session.user._id,
+      proxy.done('blockings', function(rels) {
+        return _.pluck(rels, 'block_id');
+      })
+    );
   } else {
-    Topic.getTopicsByQuery({}, options, proxy.done('topics', function (topics) {
-      return topics;
-    }));
+    proxy.emit('blockings');
   }
+
   // 取排行榜上的用户
   if (mcache.get('tops')) {
     proxy.emit('tops', mcache.get('tops'));
@@ -69,27 +115,5 @@ exports.index = function (req, res, next) {
         return tops;
       })
     );
-  }
-  // 取0回复的主题
-  if (mcache.get('no_reply_topics')) {
-    proxy.emit('no_reply_topics', mcache.get('no_reply_topics'));
-  } else {
-    Topic.getTopicsByQuery(
-      { reply_count: 0 },
-      { limit: 5, sort: [ [ 'create_at', 'desc' ] ] },
-      proxy.done('no_reply_topics', function (no_reply_topics) {
-        mcache.put('no_reply_topics', no_reply_topics, 1000 * 60 * 1);
-        return no_reply_topics;
-      }));
-  }
-  // 取分页数据
-  if (mcache.get('pages')) {
-    proxy.emit('pages', mcache.get('pages'));
-  } else {
-    Topic.getCountByQuery({}, proxy.done(function (all_topics_count) {
-      var pages = Math.ceil(all_topics_count / limit);
-      mcache.put('pages', pages, 1000 * 60 * 1);
-      proxy.emit('pages', pages);
-    }));
   }
 };
