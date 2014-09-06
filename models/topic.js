bfa78d8dbeb83516695265c5e0d445cfad611826
alsotang <alsotang@gmail.com>
2014-09-06 22:24:42 +0800

    首页的topic列表中显示来自什么版块

--- models/topic.js
+++ models/topic.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
+var config = require('../config');
+var _ = require('lodash');
 
 var TopicSchema = new Schema({
   title: { type: String },
@@ -22,5 +24,17 @@ TopicSchema.index({create_at: -1});
 TopicSchema.index({top: -1, last_reply_at: -1});
 TopicSchema.index({last_reply_at: -1});
 TopicSchema.index({author_id: 1, create_at: -1});
+
+TopicSchema.virtual('tabName').get(function () {
+  var tab = this.tab;
+  var pair = _.find(config.tabs, function (_pair) {
+    return _pair[0] === tab;
+  });
+  if (pair) {
+    return pair[1];
+  } else {
+    return '';
+  }
+});
 
 mongoose.model('Topic', TopicSchema);
