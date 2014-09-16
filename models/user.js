385b03c21d42623bb2ba7e3404297547cca551e9
alsotang <alsotang@gmail.com>
2014-09-16 22:47:27 +0800

    默认为流式评论布局

--- models/user.js
+++ models/user.js
@@ -17,7 +17,7 @@ var UserSchema = new Schema({
   githubId: { type: String},
   githubUsername: {type: String},
   is_block: {type: Boolean, default: false},
-  replys_type: {type: Number, default: 0}, // 0 为嵌套式，1 为流式
+  replys_type: {type: Number, default: 1}, // 0 为嵌套式，1 为流式
 
   score: { type: Number, default: 0 },
   topic_count: { type: Number, default: 0 },
