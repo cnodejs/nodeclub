b5b9bec86d8906a9ff9e9049eebc3417c9d0b17b
alsotang <alsotang@gmail.com>
2014-09-21 13:55:06 +0800

    不能帮自己点赞

--- controllers/reply.js
+++ controllers/reply.js
@@ -8,6 +8,7 @@ var EventProxy = require('eventproxy');
 var User = require('../proxy').User;
 var Topic = require('../proxy').Topic;
 var Reply = require('../proxy').Reply;
+var config = require('../config');
 
 /**
  * 添加一级回复
@@ -211,21 +212,29 @@ exports.up = function (req, res, next) {
     if (err) {
       return next(err);
     }
-    var action;
-    reply.ups = reply.ups || [];
-    var upIndex = reply.ups.indexOf(userId);
-    if (upIndex === -1) {
-      reply.ups.push(userId);
-      action = 'up';
-    } else {
-      reply.ups.splice(upIndex, 1);
-      action = 'down';
-    }
-    reply.save(function () {
+    if (reply.author_id.equals(userId) && !config.debug) {
+      // 不能帮自己点赞
       res.send({
-        success: true,
-        action: action
+        success: false,
+        message: '呵呵，不能帮自己点赞。',
       });
-    });
+    } else {
+      var action;
+      reply.ups = reply.ups || [];
+      var upIndex = reply.ups.indexOf(userId);
+      if (upIndex === -1) {
+        reply.ups.push(userId);
+        action = 'up';
+      } else {
+        reply.ups.splice(upIndex, 1);
+        action = 'down';
+      }
+      reply.save(function () {
+        res.send({
+          success: true,
+          action: action
+        });
+      });
+    }
   });
 };
