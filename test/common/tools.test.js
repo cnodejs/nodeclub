4ea730f78f9fa90839cddb5fca07918ab43585e6
alsotang <alsotang@gmail.com>
2014-09-21 23:27:40 +0800

    fix test

--- test/common/tools.test.js
+++ test/common/tools.test.js
@@ -11,7 +11,7 @@ var tools = require('../../common/tools');
 
 describe('test/common/tools.test.js', function () {
   it('should format date', function () {
-    tools.formatDate(new Date(0)).should.equal('1970-01-01 08:00');
+    tools.formatDate(new Date(0)).should.match(/1970\-01\-01 0\d:00/);
   });
   it('should format date friendly', function () {
     tools.formatDate(new Date(), true).should.equal('几秒前');
