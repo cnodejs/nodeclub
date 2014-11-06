df8b4163e1c5c622f1c4a039fd7f770b9024b002
alsotang <alsotang@gmail.com>
2014-11-07 00:36:20 +0800

    accessToken 接口返回头像

--- api/v1/tools.js
+++ api/v1/tools.js
@@ -6,6 +6,10 @@ var accesstoken = function (req, res, next) {
   var ep = new eventproxy();
   ep.fail(next);
 
-  res.send({success: true, loginname: req.user.loginname});
+  res.send({
+    success: true,
+    loginname: req.user.loginname,
+    avatar_url: req.user.avatar_url,
+  });
 };
 exports.accesstoken = accesstoken;
