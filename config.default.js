059533316748f79db3f4604946bffdd7034d7755
alsotang <alsotang@gmail.com>
2015-01-21 00:28:15 +0800

    add cnzz analytics

--- config.default.js
+++ config.default.js
@@ -30,7 +30,9 @@ var config = {
   // 社区的域名
   host: 'localhost',
   // 默认的Google tracker ID，自有站点请修改，申请地址：http://www.google.com/analytics/
-  google_tracker_id: 'UA-4175xxxx-x',
+  google_tracker_id: '',
+  // 默认的cnzz tracker ID，自有站点请修改
+  cnzz_tracker_id: '',
 
   // mongodb 配置
   db: 'mongodb://127.0.0.1/node_club_dev',
