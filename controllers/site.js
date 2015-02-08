63a88111c24eb6b9ee726629a8fca2f64b01394f
Lance Li <lyfuture@gmail.com>
2015-02-08 23:15:25 +0800

    在页面右侧增加客户端下载二维码

--- controllers/site.js
+++ controllers/site.js
@@ -174,3 +174,11 @@ exports.sitemap = function (req, res, next) {
     }
   }));
 };
+
+exports.appDownload = function (req, res, next) {
+  if (/Android/i.test(req.headers['user-agent'])) {
+    res.redirect('http://fir.im/ks4u');
+  } else {
+    res.redirect('https://itunes.apple.com/cn/app/id954734793');
+  }
+};
--- web_router.js
+++ web_router.js
@@ -31,6 +31,8 @@ var router = express.Router();
 router.get('/', site.index);
 // sitemap
 router.get('/sitemap.xml', site.sitemap);
+// app download
+router.get('/app/download', site.appDownload);
 
 // sign controller
 if (config.allow_sign_up) {
