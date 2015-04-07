825a932d7ac14d247789d19f3a43b7cb69d610d0
alsotang <alsotang@gmail.com>
2015-04-07 21:30:26 +0800

    api topic 支持取精华帖

--- controllers/site.js
+++ controllers/site.js
@@ -27,7 +27,7 @@ exports.index = function (req, res, next) {
   proxy.fail(next);
 
   // 取主题
-  var query = {deleted: false};
+  var query = {};
   if (tab && tab !== 'all') {
     if (tab === 'good') {
       query.good = true;
