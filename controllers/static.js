e0043017b1506e073f5ba331e4e73a8afa4b3ce8
Jason Lee <huacnlee@gmail.com>
2015-05-23 23:02:33 +0800

    修正代码，之前理解错误了;

--- controllers/static.js
+++ controllers/static.js
@@ -16,17 +16,17 @@ exports.getstart = function (req, res) {
   res.render('static/getstart');
 };
 
-/**
- * See http://www.robotstxt.org/robotstxt.html for documentation on how to use the robots.txt file
- * 
- * To ban all spiders from the entire site uncomment the next two lines:
- * User-Agent: *
- * Disallow: /
- */
+
 exports.robots = function (req, res, next) {
   res.type('text/plain');
   res.send(multiline(function () {;
-
+/*
+# See http://www.robotstxt.org/robotstxt.html for documentation on how to use the robots.txt file
+# 
+# To ban all spiders from the entire site uncomment the next two lines:
+# User-Agent: *
+# Disallow: /
+*/
   }));
 };
 
