5e87478c7a5466a13a3b472fe72c988584186586
Jason Lee <huacnlee@gmail.com>
2015-05-23 19:56:56 +0800

    tidy code

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,11 +11,13 @@ var UserProxy = require('../proxy').User;
  */
 exports.adminRequired = function (req, res, next) {
   if (!req.session.user) {
-    return res.render('notify/notify', {error: '你还没有登录。'});
+    return res.render('notify/notify', { error: '你还没有登录。' });
   }
+
   if (!req.session.user.is_admin) {
-    return res.render('notify/notify', {error: '需要管理员权限。'});
+    return res.render('notify/notify', { error: '需要管理员权限。' });
   }
+
   next();
 };
 
@@ -26,6 +28,7 @@ exports.userRequired = function (req, res, next) {
   if (!req.session || !req.session.user) {
     return res.status(403).send('forbidden!');
   }
+
   next();
 };
 
@@ -34,6 +37,7 @@ exports.blockUser = function () {
     if (req.path === '/signout') {
       return next();
     }
+
     if (req.session.user && req.session.user.is_block && req.method !== 'GET') {
       return res.status(403).send('您已被管理员屏蔽了。有疑问请联系 @alsotang。');
     }
@@ -44,8 +48,13 @@ exports.blockUser = function () {
 
 function gen_session(user, res) {
   var auth_token = user._id + '$$$$'; // 以后可能会存储更多信息，用 $$$$ 来分隔
-  res.cookie(config.auth_cookie_name, auth_token,
-    {path: '/', maxAge: 1000 * 60 * 60 * 24 * 30, signed: true, httpOnly: true}); //cookie 有效期30天
+  var opts = {
+    path: '/',
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+    signed: true,
+    httpOnly: true
+  };
+  res.cookie(config.auth_cookie_name, auth_token, opts); //cookie 有效期30天
 }
 
 exports.gen_session = gen_session;
@@ -63,12 +72,12 @@ exports.authUser = function (req, res, next) {
     }
     return next();
   }
-  
+
   // Ensure current_user always has defined.
   res.locals.current_user = null;
 
   ep.all('get_user', function (user) {
-    if (!user) {  
+    if (!user) {
       return next();
     }
     user = res.locals.current_user = req.session.user = new UserModel(user);
@@ -76,16 +85,17 @@ exports.authUser = function (req, res, next) {
     if (config.admins.hasOwnProperty(user.loginname)) {
       user.is_admin = true;
     }
+    
     Message.getMessagesCount(user._id, ep.done(function (count) {
       user.messages_count = count;
       next();
     }));
-
   });
 
   if (req.session.user) {
     ep.emit('get_user', req.session.user);
-  } else {
+  }
+  else {
     var auth_token = req.signedCookies[config.auth_cookie_name];
     if (!auth_token) {
       return next();
