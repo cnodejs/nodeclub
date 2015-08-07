a3cc3280f4c9c60cb435deb923cf8872af9fc1dc
alsotang <alsotang@gmail.com>
2015-08-07 18:28:18 +0800

    mongodb poolSize 20

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 var config   = require('../config');
 
-mongoose.connect(config.db, function (err) {
+mongoose.connect(config.db, {
+  server: {poolSize: 20}
+}, function (err) {
   if (err) {
     console.error('connect to %s error: ', config.db, err.message);
     process.exit(1);
