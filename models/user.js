27ad50c102477cfb43885481c8653ea50af34d53
alsotang <alsotang@gmail.com>
2015-08-12 10:43:00 +0800

    gravatar 使用 https

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ var BaseModel = require("./base_model");
 var renderHelper = require('../common/render_helper');
 var Schema    = mongoose.Schema;
 var utility   = require('utility');
+var _ = require('lodash');
 
 var UserSchema = new Schema({
   name: { type: String},
@@ -48,7 +49,13 @@ UserSchema.plugin(BaseModel);
 UserSchema.virtual('avatar_url').get(function () {
   var url = this.avatar || ('https://gravatar.com/avatar/' + utility.md5(this.email.toLowerCase()) + '?size=48');
 
+  // www.gravatar.com 被墙
   url = url.replace('www.gravatar.com', 'gravatar.com');
+
+  // 让协议自适应 protocol，使用 `//` 开头
+  if (url.indexOf('http:') === 0) {
+    url = url.slice(5);
+  }
 
   // 如果是 github 的头像，则限制大小
   if (url.indexOf('githubusercontent') !== -1) {
