774427a99cd3f7bc5aebd034a45d92380d8530a3
alsotang <alsotang@gmail.com>
2016-05-05 12:59:08 +0800

    推荐客户端改为 noder

--- controllers/site.js
+++ controllers/site.js
@@ -143,9 +143,5 @@ exports.sitemap = function (req, res, next) {
 };
 
 exports.appDownload = function (req, res, next) {
-  if (/Android/i.test(req.headers['user-agent'])) {
-    res.redirect('http://fir.im/ks4u');
-  } else {
-    res.redirect('https://itunes.apple.com/cn/app/id954734793');
-  }
+  res.redirect('https://github.com/soliury/noder-react-native/blob/master/README.md')
 };
