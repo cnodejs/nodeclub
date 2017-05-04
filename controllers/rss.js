069a5c4169644d49c784e495dd71bcaaefba42f0
alsotang <alsotang@gmail.com>
2017-05-04 16:42:17 +0800

    过滤 xml 不允许的 utf8 字符

--- controllers/rss.js
+++ controllers/rss.js
@@ -47,10 +47,14 @@ exports.index = function (req, res, next) {
         });
 
         var rssContent = convert('rss', rss_obj);
-
+        rssContent = utf8ForXml(rssContent)
         cache.set('rss', rssContent, 60 * 5); // 五分钟
         res.send(rssContent);
       });
     }
   }));
 };
+
+function utf8ForXml(inputStr) {
+  return inputStr.replace(/[^\x09\x0A\x0D\x20-\xFF\x85\xA0-\uD7FF\uE000-\uFDCF\uFDE0-\uFFFD]/gm, '');
+}
