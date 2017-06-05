c813ec5a7982196de21685ce623be0b0d56e5ff0
alsotang <alsotang@gmail.com>
2017-06-05 14:56:37 +0800

    无人回复的话题忽略 dev 版块

--- controllers/site.js
+++ controllers/site.js
@@ -68,7 +68,7 @@ exports.index = function (req, res, next) {
       proxy.emit('no_reply_topics', no_reply_topics);
     } else {
       Topic.getTopicsByQuery(
-        { reply_count: 0, tab: {$ne: 'job'}},
+        { reply_count: 0, tab: {$nin: ['job', 'dev']}},
         { limit: 5, sort: '-create_at'},
         proxy.done('no_reply_topics', function (no_reply_topics) {
           cache.set('no_reply_topics', no_reply_topics, 60 * 1);
